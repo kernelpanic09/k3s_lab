e42f403664ec61c189a7deb1f56d77a46d87fc67
kernelpanic09 <redcom969@gmail.com>
2025-06-09 03:24:07 +0000

    app_update

--- apps/demoapp/app.py
+++ apps/demoapp/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD pipeline!!"
+    return "Hello from CI/CD - this is a TESSSSSSSSSSSSSSSSSt"
 
 @app.route("/health")
 def health():
